f88286086aa65c125bf787dcd53ede28660ab530
wangyu- <wangyucn@gmail.com>
2019-07-25 22:03:55 -0500

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-MY_IP="192.168.99.176"
+MY_IP="192.168.22.135"
 MY_PORT="8000"
 TG_IP="45.76.100.53"
 TG_PORT="80"
@@ -7,4 +7,4 @@ make
 rmmod mynat.ko
 insmod mynat.ko my_ip=$MY_IP my_port=$MY_PORT tg_ip=$TG_IP tg_port=$TG_PORT
 
-ping -c 10 45.76.100.53 >/dev/null 2>&1 &
+ping -c 10 $TG_IP >/dev/null 2>&1 &
